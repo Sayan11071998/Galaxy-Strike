59dfc0050b2d91e032dc6e45d5ea385c573d0805
Sayan Nandi <sayannandi11071998@gmail.com>
2025-04-21 01:02:54 +0530

    Scoreboard logic Implemented

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,10 +3,30 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private GameObject destroyedVFX;
+    [SerializeField] private int hitPoints = 3;
+    [SerializeField] private int scoreValue = 10;
+
+    private Scoreboard scoreboard;
+
+    private void Start()
+    {
+        scoreboard = FindFirstObjectByType<Scoreboard>();
+    }
 
     private void OnParticleCollision(GameObject other)
     {
-        Instantiate(destroyedVFX, transform.position, Quaternion.identity);
-        Destroy(this.gameObject);
+        ProcessHit();
+    }
+
+    private void ProcessHit()
+    {
+        hitPoints--;
+
+        if (hitPoints <= 0)
+        {
+            scoreboard.IncreaseScore(scoreValue);
+            Instantiate(destroyedVFX, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
     }
 }
