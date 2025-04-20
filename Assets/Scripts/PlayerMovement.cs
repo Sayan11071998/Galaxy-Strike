7f60787cb0a0fe1ca441f2890367a2f67fa4b733
Sayan Nandi <sayannandi11071998@gmail.com>
2025-04-20 12:11:07 +0530

    Working on Player Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float controlSpeed = 10f;
+    [SerializeField] private float xClampRange = 5f;
+    [SerializeField] private float yClampedRange = 5f;
 
     private Vector2 movement;
 
@@ -20,7 +22,13 @@ public class PlayerMovement : MonoBehaviour
     private void ProcessTranslation()
     {
         float xOffset = movement.x * controlSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xClampRange, xClampRange);
+
         float yOffset = movement.y * controlSpeed * Time.deltaTime;
-        transform.localPosition = new Vector3(transform.localPosition.x + xOffset, transform.localPosition.y + yOffset, 0f);
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yClampedRange, yClampedRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, 0f);
     }
 }
