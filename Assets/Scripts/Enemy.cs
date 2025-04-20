edabd7fc4b189a24b95d32893e77bed356d921af
Sayan Nandi <sayannandi11071998@gmail.com>
2025-04-20 22:08:18 +0530

    Enemy Destroy VFX created

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private GameObject destroyedVFX;
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Instantiate(destroyedVFX, transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
+    }
+}
