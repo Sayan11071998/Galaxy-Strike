631f3e9f2334ba646008a3662594b8138d03cec1
Sayan Nandi <sayannandi11071998@gmail.com>
2025-04-20 21:43:55 +0530

    Weapon Look At target achieved

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -5,6 +5,8 @@ public class PlayerWeapon : MonoBehaviour
 {
     [SerializeField] private GameObject[] lasers;
     [SerializeField] private RectTransform crosshair;
+    [SerializeField] private Transform targetPoint;
+    [SerializeField] private float targetDistance = 100f;
 
     private bool isFiring = false;
 
@@ -17,6 +19,8 @@ public class PlayerWeapon : MonoBehaviour
     {
         ProcessFiring();
         MoveCrosshair();
+        MoveTargetPoint();
+        AimLasers();
 
     }
 
@@ -37,5 +41,21 @@ public class PlayerWeapon : MonoBehaviour
     private void MoveCrosshair()
     {
         crosshair.position = Input.mousePosition;
+    }
+
+    private void MoveTargetPoint()
+    {
+        Vector3 targetPointPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, targetDistance);
+        targetPoint.position = Camera.main.ScreenToWorldPoint(targetPointPosition);
+    }
+
+    private void AimLasers()
+    {
+        foreach (GameObject laser in lasers)
+        {
+            Vector3 fireDirection = targetPoint.position - this.transform.position;
+            Quaternion rotationToTarget = Quaternion.LookRotation(fireDirection);
+            laser.transform.rotation = rotationToTarget;
+        }
     }
 }
